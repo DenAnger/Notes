7faee6725afd7a6abf4603b07c2d1c8e049b7d27
Denis Abramov <DenAnger@outlook.com>
2021-03-12 13:09:48 +0700

    note model
    add Entity, note model
    bug fix

---
+++ Notes/Models/Note.swift
@@ -0,0 +1,29 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Denis Abramov on 12.03.2021.
+//
+
+import Foundation
+
+class Note {
+    private(set) var noteID: UUID
+    private(set) var noteTitle: String
+    private(set) var noteText: String
+    private(set) var noteTimeStamp: Int64
+    
+    init(id: UUID, title: String, text: String, timeStamp: Int64) {
+        self.noteID = id
+        self.noteTitle = title
+        self.noteText = text
+        self.noteTimeStamp = timeStamp
+    }
+    
+    init(title: String, text: String, timeStamp: Int64) {
+        self.noteID = UUID()
+        self.noteTitle = title
+        self.noteText = text
+        self.noteTimeStamp = timeStamp
+    }
+}
