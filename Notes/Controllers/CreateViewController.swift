402f08d71d57a93568020d3b5c1b972c70e04789
Denis Abramov <DenAnger@outlook.com>
2021-03-12 12:06:19 +0700

    interface CreateViewController
    add UI Elements.

---
+++ Notes/Controllers/CreateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Denis Abramov on 11.03.2021.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var createTextView: UITextView!
+    @IBOutlet var saveButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
