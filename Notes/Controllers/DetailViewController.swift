4e480baac44a75b92acdd7e85c4ad9be53ead9ab
Denis Abramov <DenAnger@outlook.com>
2021-03-12 12:23:20 +0700

    interface DetailViewController
    add UI Elements
    bug fix

---
+++ Notes/Controllers/DetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailViewController.swift
+//  Notes
+//
+//  Created by Denis Abramov on 12.03.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
