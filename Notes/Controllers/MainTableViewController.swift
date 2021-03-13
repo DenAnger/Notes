d476e389b58c3f6de7463254bfb9b1e0225ecc34
Denis Abramov <DenAnger@outlook.com>
2021-03-13 23:04:29 +0700

    MainTableViewController
    add methods

--- Notes/Controllers/MainTableViewController.swift
+++ Notes/Controllers/MainTableViewController.swift
@@ -8,87 +8,82 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
+    
+    var detailViewController: DetailViewController? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//         self.navigationItem.rightBarButtonItem = self.editButtonItem
+        configurateCoreData()
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add,
+                                        target: self,
+                                        action: #selector(insertNewObject(_:)))
+         navigationItem.rightBarButtonItem = addButton
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return Storage.storage.count()
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell",
+                                                 for: indexPath) as! NoteCell
 
-        // Configure the cell...
-
+        if let object = Storage.storage.readNote(at: indexPath.row) {
+            cell.titleLabel!.text = object.noteTitle
+            cell.noteTextLabel!.text = object.noteText
+            cell.dateLabel!.text = DateHelper.convertDate(
+                date: Date.init(seconds: object.noteTimeStamp)
+            )
+        }
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+    override func tableView(_ tableView: UITableView,
+                            canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCell.EditingStyle,
+                            forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            Storage.storage.removeNote(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     // MARK: - Table View delegate
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Methods
+    
+    func configurateCoreData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            let alert = UIAlertController(
+                title: "Couldn't get app delegate",
+                message: "Couldn't get app delegate, unexpected error occurred. Try again later",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            self.present(alert, animated: true)
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        Storage.storage.setManagedContext(context: managedContext)
     }
-    */
-
+    
+    @objc func insertNewObject(_ sender: Any) {
+        performSegue(withIdentifier: "createNote", sender: self)
+    }
 }
--- Notes/Views/NoteCell.swift
+++ Notes/Views/NoteCell.swift
@@ -8,5 +8,11 @@
 import UIKit
 
 class NoteCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteTextLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
     
+    private(set) var noteTitle = ""
+    private(set) var noteText = ""
+    private(set) var noteDate = ""
 }
